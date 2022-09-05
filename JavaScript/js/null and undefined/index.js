8b21c500feac2349a68ad5bf199bc0477d7c0353
prasannarames <prasannaramesh98@gmail.com>
2022-09-05 13:53:07 +0530

    docs: null and undefined

--- JavaScript/js/null and undefined/index.js
+++ JavaScript/js/null and undefined/index.js
@@ -14,6 +14,7 @@ console.log(!!undefined); // or Boolean(undefined)
 /// null is just a special value that represents 'empty' or 'nothing' or 'value unknown'
 /// null consider as a system level
 /// if we want to explicit emptyness in some of our variable or code we can use null
+/// console.log(typeof null); // object
 /// example
 let a = null; 
 
@@ -23,8 +24,21 @@ let a = null;
 /// if a variable is declared but not assigned then it is undefined
 /// undefined consider as a program level
 /// undefined also used explicitly but it is not recommended. (undefined works implicitly)
+/// console.log(typeof undefined); // undefined
 /// example
-let b;
-console.log(b)// undefined 
+/// let b;
+/// console.log(b)// undefined 
+
+let _thisIsUndefined;
+const doNothing = () => {};
+const someObj = {
+  a : "ay",
+  b : "bee",
+  c : "si"
+};
+
+console.log(_thisIsUndefined); //logs undefined
+console.log(doNothing()); //logs undefined
+console.log(someObj["d"]); //logs undefined
 
 
