4265764eeb9e7692aaa86729ef75e9833ea27c6b
prasannarames <prasannaramesh98@gmail.com>
2022-09-16 12:32:47 +0530

    docs: resources

---
+++ JavaScript/js/operators/index.js
@@ -0,0 +1,2 @@
+// refer this link for operators 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
---
+++ JavaScript/js/operators/logicalOperators.js
@@ -0,0 +1,11 @@
+// const a = 5 
+// const b = 6
+
+// if(a < b && a <=b){
+//     console.log(" a is valid")
+// } else {
+//     console.log("b is valid")
+// }
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
